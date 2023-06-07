8085bfda70031314ac8cbd823c8dd1524a8e72e4
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-06-07 11:25:43 +0530

    show booking requests

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -34,10 +34,40 @@ router.post('/create_booking',(req,res)=>{
             msg:'Not Authorized to Make booking requests'
         })
     }
+})
 
 
+
+
+// Show all the booking requests
+router.get('/show_booking_requests',async (req,res)=>{
+    
+    if(req.isAuthenticated() && req.user.type === 'Admin'){
+        try{
+            const requests = await Booking_Requests.
+            find().
+            populate('department').
+            exec();
+            res.send({
+                booking_requests:requests
+            })
+        }
+        catch(err){
+            res.send({
+                error:err
+            })
+        }
+    }
+    else{
+        res.send({
+            msg:'You are not authorized'
+        })
+    }
 
 })
+
+
+
 
 
 
