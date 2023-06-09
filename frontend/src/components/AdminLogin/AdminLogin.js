87931425004f07d8d6a9ef91c4d6de33902bce3e
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-06-09 12:15:34 +0530

    Admin Login card completed

--- frontend/src/components/AdminLogin/AdminLogin.js
+++ frontend/src/components/AdminLogin/AdminLogin.js
@@ -6,10 +6,18 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
-import OutlinedInput from '@mui/material/OutlinedInput';
+import Input from '@mui/material/Input';
 import InputAdornment from '@mui/material/InputAdornment';
+import EmailIcon from '@mui/icons-material/Email';
+import HttpsIcon from '@mui/icons-material/Https';
+
+
+const handleSubmit = (e)=>{
+    // e.preventDefault()
+    console.log('Hello')
+}
+
 
 export default function AdminLogin() {
   return (
@@ -17,7 +25,7 @@ export default function AdminLogin() {
     <div className='admin-login-body'>
 
     <Grid container spacing={2} style={{height:'100%'}} alignContent={'center'} justifyContent={'center'}>
-        <Grid item xs={5}>
+        <Grid item xs={11} sm={8} md={7} lg={6} xl={5}>
 
             <Card sx={{}} className='admin-card'>
                 <CardContent>
@@ -29,48 +37,66 @@ export default function AdminLogin() {
 
 
 
+                        <form onSubmit={handleSubmit}>
                 {/* Input */}
-                <Grid container spacing={2} justifyContent={'center'} alignContent={'center'}>
+                <Grid container spacing={4} justifyContent={'center'} alignContent={'center'}>
 
-                        <Grid item xs={9} justifyItems={'center'}>
-                                <FormControl fullWidth   variant="outlined">
-                                    <OutlinedInput
-                                        id="outlined-adornment-weight"
-                                        className='admin-input'
-                                        aria-describedby="outlined-weight-helper-text"
-                                        inputProps={{
-                                        'aria-label': 'weight',
-                                        }}
-                                    />
+
+                        <Grid item  xs={11} sm={9} md={8} lg={9} xl={9} justifyItems={'center'}>
+
+                                <FormControl fullWidth> 
+                                     <Input
+                                     disableUnderline={true}
+                                     type='email'
+                                     placeholder="Email"
+                                     required={true}
+                                     className='admin-input'
+                                     startAdornment={
+                                    <InputAdornment position="start" sx={{marginLeft:'0.5rem'}}>
+                                        <EmailIcon/>
+                                    </InputAdornment>
+                                }
+                                 />
                             </FormControl>
 
                         </Grid>
 
 
-                        <Grid item xs={9}>
+                        <Grid item xs={11} sm={9} md={8} lg={9} xl={9}>
 
-                        <FormControl sx={{  }} variant="outlined" fullWidth>
-                            <OutlinedInput
-                                id="outlined-adornment-weight"
-                                className='admin-input'
-                                aria-describedby="outlined-weight-helper-text"
-                                inputProps={{
-                                'aria-label': 'weight',
-                                }}
-                                style={{marginBottom:'5rem'}}
-                            />
+                        <FormControl  fullWidth error>
+                             <Input
+                                     type='password'
+                                     placeholder="Password"
+                                     required={true}
+                                     disableUnderline={true}
+                                     className='admin-input'
+                                     startAdornment={
+                                    <InputAdornment position="start" sx={{marginLeft:'0.5rem'}}>
+                                       <HttpsIcon/>
+                                    </InputAdornment>
+                                }
+                                 />
                     </FormControl>
 
                         </Grid>
+
+
+                        <Grid item xs={8} sm={7} md={6} lg={6} xl={5}>
+                         <Button size="medium" type='submit' className='btn-login-card' fullWidth>LOGIN HERE</Button>
+                        </Grid>
+
+
+
                 </Grid>
-
-
+                
+                        </form>
             
 
 
 
                 <CardActions>
-                    {/* <Button size="small">Learn More</Button> */}
+
                 </CardActions>
             </Card>
 
@@ -79,13 +105,8 @@ export default function AdminLogin() {
 
         </Grid>
       </Grid>
-
-
-
-
     </div>
-
-
+    
    </>
   )
 }
