add5f61ec1c9a44b1727948e7392dcf8be532193
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-06-09 10:31:46 +0530

    admin card added

--- frontend/src/components/AdminLogin/AdminLogin.js
+++ frontend/src/components/AdminLogin/AdminLogin.js
@@ -1,11 +1,45 @@
 import React from 'react'
 import "./AdminLogin.css"
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 export default function AdminLogin() {
   return (
    <>
     <div className='admin-login-body'>
-        Hello
+
+    <Grid container spacing={2}>
+        <Grid item xs={8}>
+
+            <Card sx={{}} className='admin-card'>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+
+
+
+
+        </Grid>
+      </Grid>
+
+
+
+
     </div>
 
 
