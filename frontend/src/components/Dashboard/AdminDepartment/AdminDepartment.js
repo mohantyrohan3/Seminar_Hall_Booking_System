2a788029cd424ca5de1467f46ef20e0b1e6f58f9
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-06-10 12:25:25 +0530

    admin department completed

--- frontend/src/components/Dashboard/AdminDepartment/AdminDepartment.js
+++ frontend/src/components/Dashboard/AdminDepartment/AdminDepartment.js
@@ -11,13 +11,54 @@ import { CardActionArea } from '@mui/material';
 import Person2Icon from '@mui/icons-material/Person2';
 import EmailIcon from '@mui/icons-material/Email';
 
+
+const pages = [1,2,3,4];
+
+
+
+
 export default function AdminDepartment() {
+
+    const list = pages.map(()=>{
+        return(
+            <Grid item xs={11} sm={6} md={6} lg={6} xl={6}>
+            <Card sx={{}} className='admin-department-card'>
+            <CardActionArea>
+                <CardContent>
+
+                <Typography gutterBottom variant="h5" className='admin-department-text' component="div">
+                    DEPARTMENT NAME
+                </Typography>
+                <br/>
+                <Typography variant="body2"className='admin-department-text'>
+                    <Person2Icon sx={{marginRight:"1rem"}}/>
+                    HEAD OF THE DEPARTMENT
+                </Typography>
+                <br/>
+                
+                <Typography variant="body2"className='admin-department-text'>
+                    <EmailIcon sx={{marginRight:"1rem"}}/>
+                        EMAIL
+                </Typography>                               
+
+                </CardContent>
+            </CardActionArea>
+            </Card>
+            </Grid>
+        )
+    })
+
+
+
+
+
+
   return (
   <>
     <div className='admin-department-body'>
         <Appbar/>
         
-        <Grid container justifyContent={'space-around'}>
+                    <Grid container justifyContent={'space-around'}>
                 
                     <Grid item xs={11} sm={5} md={4} lg={4} xl={3} >
                         <div className='admin-department-title-div'>
@@ -29,48 +70,16 @@ export default function AdminDepartment() {
                     <Button size="medium" fullWidth className='btn-admin-department'>CREATE DEPARTMENT</Button>
                     </Grid>
                 
-                </Grid>
+                    </Grid>
 
 
 
 
             <Container>
-            <Grid container spacing={2}>
-
-                    <Grid item xs={4}>
-                    <Card sx={{}} className='admin-department-card'>
-                            <CardActionArea>
-                                <CardContent>
-
-                                <Typography gutterBottom variant="h5" className='admin-department-text' component="div">
-                                    DEPARTMENT NAME
-                                </Typography>
-                                <br/>
-                                <Typography variant="body2"className='admin-department-text'>
-                                    <Person2Icon sx={{marginRight:"1rem"}}/>
-                                    HEAD OF THE DEPARTMENT
-                                </Typography>
-                                <br/>
-                                
-                                <Typography variant="body2"className='admin-department-text'>
-                                    <EmailIcon sx={{marginRight:"1rem"}}/>
-                                        EMAIL
-                                </Typography>                               
-                                
-                                </CardContent>
-                            </CardActionArea>
-                     </Card>
+                    <Grid container spacing={2} justifyContent={'center'}>
+                        {list}
                     </Grid>
-
-
-            </Grid>
             </Container>
-
-
-
-
-
-
 
     </div>
   </>
