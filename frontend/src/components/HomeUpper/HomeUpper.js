c8af5be0c2835f0c1064ab9f5e05a573c02beaca
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-06-11 09:45:10 +0530

    update on home navbar

--- frontend/src/components/HomeUpper/HomeUpper.js
+++ frontend/src/components/HomeUpper/HomeUpper.js
@@ -13,7 +13,7 @@ export default function HomeUpper() {
 
     <Box>
     <Grid container justifyContent={'center'}>
-        <Grid item xs={12} sm={8} md={8} lg={7} xl={6}   className='navbar'>
+        <Grid item xs={11} sm={8} md={8} lg={7} xl={6}   className='navbar'>
           <div className='list-item'>
             <div className='list-item-children'>
             <Link to="/">HOME</Link>
