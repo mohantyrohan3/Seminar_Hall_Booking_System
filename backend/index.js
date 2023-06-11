5ade71154c1377a3844181fab553ce865a69fb99
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-06-11 12:23:55 +0530

    server updated

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,9 @@ app.use(session({
     saveUninitialized: true,
     cookie: { maxAge:1000*60*60*24 },
     store: MongoStore.create({ mongoUrl: `mongodb+srv://mohantyrohan:${details.MONGO_Password}@cluster0.llzjwsh.mongodb.net/seminar_hall_DB?retryWrites=true&w=majority`,collectionName:"sessions" }),
-    domain: 'localhost'
+    cookie: {
+        domain: 'localhost',
+    }
   }))
 
 
