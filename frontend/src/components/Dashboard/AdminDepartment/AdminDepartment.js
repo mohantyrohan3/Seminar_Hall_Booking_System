95eb2f1e6895ac57a2349de366e19d94868478b0
Rohan Mohanty <mohantyrohan3@gmail.com>
2023-06-13 21:10:25 +0530

    create department completed

--- frontend/src/components/Dashboard/AdminDepartment/AdminDepartment.js
+++ frontend/src/components/Dashboard/AdminDepartment/AdminDepartment.js
@@ -13,8 +13,11 @@ import EmailIcon from '@mui/icons-material/Email';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 import axios from 'axios';
-
-
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import {createDepartmentApi} from "../../../api/createdepartmentapi"
 
 
 
@@ -23,10 +26,30 @@ export default function AdminDepartment() {
 
     const [open, setOpen] = useState(true);
     const [list, setlist] = useState([]);
+    const [modal, setmodal] = useState(false);
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState('');
+    const [department, setdepartment] = useState("");
+    const [head, sethead] = useState('');
+
+
+    const createDepartment = ()=>{
+        setmodal(true)
+      }
+
+      const handleClose = () =>{
+       setmodal(false)
+       setemail("");
+       setpassword("");
+       setdepartment("");
+       sethead("")
+      }
+
 
 
 
     const get_departments = async ()=> {
+        setOpen(true)
         try{
             const response = await axios.get("https://seminar.rohankm.online/api/department/show_departments")
            const temp = response.data.departments.map((data)=> {
@@ -70,6 +93,34 @@ export default function AdminDepartment() {
 
 
 
+    const handleCreateDepartmentSubmit = async (e)=>{
+        e.preventDefault()
+        setmodal(false)
+        setOpen(true)
+        const data = {
+            email:email,
+            password:password,
+            department:department,
+            head:head
+        }
+        
+        try{
+            const response = await createDepartmentApi(data);
+        }
+
+        catch(err){
+            console.log(err)
+        }
+        setemail("");
+        setpassword("");
+        setdepartment("");
+        sethead("")
+        setOpen(false)
+        get_departments()
+    }
+
+
+
 
     useEffect(() => {
         get_departments()
@@ -94,6 +145,84 @@ export default function AdminDepartment() {
         >
             <CircularProgress color="inherit" />
         </Backdrop>
+
+
+        <Modal
+        open={modal}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className='modal'>
+          <Typography className='modal-text' sx={{marginBottom:'1rem'}} variant="h6" component="h2">
+            CREATE HALL
+          </Typography>     
+
+                            <form onSubmit={handleCreateDepartmentSubmit}>
+                              
+                            
+                            <FormControl fullWidth> 
+                                     <Input
+                                     disableUnderline={true}
+                                     type='email'
+                                     placeholder="Email"
+                                     required={true}
+                                     value={email}
+                                     className='admin-input'
+                                     onChange={(e)=> setemail(e.target.value)}
+                                    sx={{padding:'1rem'}}
+                                 />
+                            </FormControl>
+                            <FormControl fullWidth> 
+                                     <Input
+                                     disableUnderline={true}
+                                     type='password'
+                                     placeholder="Password"
+                                     required={true}
+                                     value={password}
+                                     className='admin-input'
+                                     onChange={(e)=> setpassword(e.target.value)}
+                                    sx={{padding:'1rem'}}
+                                 />
+                            </FormControl>
+                            <FormControl fullWidth> 
+                                     <Input
+                                     disableUnderline={true}
+                                     type='text'
+                                     placeholder="Department Name"
+                                     required={true}
+                                     value={department}
+                                     className='admin-input'
+                                     onChange={(e)=> setdepartment(e.target.value)}
+                                    sx={{padding:'1rem'}}
+                                 />
+                            </FormControl>
+                            <FormControl fullWidth> 
+                                     <Input
+                                     disableUnderline={true}
+                                     type='text'
+                                     placeholder="Head of the department"
+                                     required={true}
+                                     value={head}
+                                     className='admin-input'
+                                     onChange={(e)=> sethead(e.target.value)}
+                                    sx={{padding:'1rem'}}
+                                 />
+                            </FormControl>
+
+
+
+
+                            <Button size="medium" fullWidth className='btn-admin-hall' type='submit'>CREATE HALL</Button>
+                          
+                            </form>
+        </Box>
+      </Modal>
+
+
+
+
+
     
     <div className='admin-department-body'>
         <Appbar/>
@@ -107,7 +236,7 @@ export default function AdminDepartment() {
                     </Grid>
 
                     <Grid item xs={11} sm={5} md={4} lg={4} xl={3} justifyItems={'flex-end'}>
-                    <Button size="medium" fullWidth className='btn-admin-department'>CREATE DEPARTMENT</Button>
+                    <Button size="medium" fullWidth className='btn-admin-department' onClick={createDepartment}>CREATE DEPARTMENT</Button>
                     </Grid>
                 
                     </Grid>
